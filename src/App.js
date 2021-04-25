7fdc2a18c2aed4a0e65f4b41bc1e1ccae300bb75
Pontus Tahr <pontus.tahir@gmail.com>
2021-04-25 21:18:24 +0200

    Products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { commerce } from './Lib/Commerce';
+import { Products, NavBar } from './components';
 
+const App = () => {
+    const [products, setProducts] = useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <h1>SHop shop</h1>
-    </div>
-  );
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    return (
+        <div>
+            <NavBar />
+            <Products products={products} />
+        </div>
+    )
 }
 
-export default App;
+export default App
